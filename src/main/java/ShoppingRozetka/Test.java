e05e41572c25bf0ce35385f8115f831d02408f37
Oleksandr-Tsymbalist <aleksandr.tsymbalist@gmail.com>
2023-03-18 18:24:29 +0200

    in progress

--- src/main/java/ShoppingRozetka/Test.java
+++ src/main/java/ShoppingRozetka/Test.java
@@ -1,13 +1,14 @@
 package ShoppingRozetka;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriverManager.chromedriver().arch64().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.youtube.com/");
-
+        driver.close();
     }
 }
